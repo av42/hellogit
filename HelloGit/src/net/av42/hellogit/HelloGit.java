7a8fce09c6ec18d6520ada614207cd58761c74e6
av42 <av247andrej@aol.de>
2017-08-27 21:47:57 +0200

    1. commit:
    Committed HelloGit project

---
+++ HelloGit/src/net/av42/hellogit/HelloGit.java
@@ -0,0 +1,10 @@
+package net.av42.hellogit;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, git!");
+	}
+
+}
